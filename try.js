3cc2c1bfc495820b1cc4a3bbbd0fdb27d96b6b7b
Daniela510 <whateverellie@gmail.com>
2019-11-18 10:15:31 +0100

    a lot
    i added a data folder with already fetched images

--- try.js
+++ try.js
@@ -1,5 +1,6 @@
 var cnv;
 var bi=[];
+let scnv;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -9,16 +10,20 @@ function centerCanvas() {
 
 function preload() {
 const div = document.getElementById('data');
-for(let i = 0; i < 3; i++) {
+for(let i = 0; i < 50; i++) {
     bi[i]= loadImage(instagram_output.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
 }
 }
 
 function setup() {
-  cnv = createCanvas(600, 600);
-  centerCanvas();
+  cnv = createCanvas(windowHeight, windowHeight,0,0);
+  //centerCanvas();
     noStroke();
-  imageMode(CORNER);
+    //randomize
+    var one = floor(random(0,50));var two = floor(random(0,50));var thr = floor(random(0,50));
+
+  imageMode(CORNER);  background(bi[two]);tint(255, 127);
+  image(bi[thr], 0, 0); image(bi[one], 0, 0);
 for (var i=0; i<100; i++){
   pixels.push(new pixel());
 }
@@ -42,16 +47,20 @@ function pixel(){
     this.x= constrain(this.x,0,width);
     this.y += dy;
     this.y = constrain(this.y,0,height);
-var r = floor(random(0,3)); //selecting the image from witch to pull pxels
+    let variables = [one, two, thr];
+var r = (random(variables)); //selecting the image from witch to pull pxels
     var color = bi[r].get(this.x, this.y); // color of pixel in image
     fill(color);
 
     ellipse(this.x, this.y, 3, 3);
   }
 }
-function windowResized() {
-  centerCanvas();
-}
+function mousePressed() {
+saveCanvas('myCanvas', 'jpg');
+  }
+// function windowResized() {
+//   centerCanvas();
+// }
 // if (mouseIsPressed) {
 //   fill(0);
 // } else {
